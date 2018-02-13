a3afbe3fda977537c45db91e8e42941820f6fcdb
Manjunatha D <manjunathad13@gmail.com>
2018-02-13 18:47:54 +0530

    minor fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Header(props) {
   return (
-    <header class="header">
-      <h1 class="title">{props.title}</h1>
-      <p class="subtitle">{props.subtitle}</p>
+    <header className="header">
+      <h1 className="title">{props.title}</h1>
+      <p className="subtitle">{props.subtitle}</p>
     </header>
   );
 }
